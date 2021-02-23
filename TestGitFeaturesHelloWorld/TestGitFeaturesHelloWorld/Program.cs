6795cbccb263d72721e485cb0b4b78bd00cb29f7
Alex Goolyuk <alex.goolyuk@gmail.com>
2021-02-23 11:59:52 +0200

    final changes before merge

--- TestGitFeaturesHelloWorld/TestGitFeaturesHelloWorld/Program.cs
+++ TestGitFeaturesHelloWorld/TestGitFeaturesHelloWorld/Program.cs
@@ -8,6 +8,7 @@ namespace TestGitFeaturesHelloWorld
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("addin feature to feature-branch");
+            Console.WriteLine("add data for merge to develop branch");
         }
     }
 }
