931f590336d9032e49231a11db1a195a9fba15bb
Alex Goolyuk <alex.goolyuk@gmail.com>
2021-02-23 11:35:06 +0200

    first commit git test

---
+++ TestGitFeaturesHelloWorld/TestGitFeaturesHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGitFeaturesHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
